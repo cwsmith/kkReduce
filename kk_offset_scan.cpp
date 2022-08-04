dc3b904e4b2df2c5725e7b97aa5fb0650ce06557
Cameron Smith <smithc11@rpi.edu>
2022-08-04 14:17:13 -0400

    offsetscan: mimic omegah mem pool

--- kk_offset_scan.cpp
+++ kk_offset_scan.cpp
@@ -1,29 +1,38 @@
 #include<Kokkos_Core.hpp>
 #include<cstdio>
 
+void test_scan(int N, Kokkos::View<int*>& src, Kokkos::View<int*>& out) {
+  assert(src.size() >= N+1);
+  assert(out.size() >= N+1);
+  std::stringstream ss;
+  ss << "offsetScan_" << N;
+  std::string name = ss.str();
+
+  int ignored = 0;
+  Kokkos::parallel_scan(name, N+1,
+      KOKKOS_LAMBDA(int i, int& partial_sum, bool is_final) {
+      if(is_final) out(i) = partial_sum;
+      partial_sum += src(i);
+      }, ignored);
+}
+
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc,argv);
   {
-    int N = argc>1?atoi(argv[1]):100;
-    Kokkos::View<int*>post("postfix_sum",N);
-    Kokkos::View<int*>pre("prefix_sum",N+1);
-    Kokkos::View<int*>src("source",N+1);
-
-    Kokkos::View<int*, Kokkos::HostSpace> host("host", N+1);
-    for(int i=0; i<host.size(); i++) host(i) = 2;
-    Kokkos::deep_copy(src, host);
-
-    int result = 0;
-    Kokkos::parallel_scan("Loop1", N+1,
-      KOKKOS_LAMBDA(int i, int& partial_sum, bool is_final) {
-      if(is_final) pre(i) = partial_sum;
-      partial_sum += src(i);
-    }, result);
-
-    Kokkos::deep_copy(host, pre);
-    printf("pre\n"); 
-    for(int i=0; i<host.size(); i++) printf("%d ", host(i));
-    printf("\n"); 
+  auto size = std::size_t(1) << 21;
+  //allocate the input and output arrays once to mimic
+  //the omega_h memory pool
+  Kokkos::View<int*> src("source",size);
+  Kokkos::parallel_for("ones", size, KOKKOS_LAMBDA(int i) { src(i)=1; });
+  Kokkos::View<int*> out("source",size);
+  for(int i=0; i<16; i++) {
+    size = size >> 1;
+    printf("%d size %lu\n", i, size);
+    for(int j=0; j<10; j++) {
+      test_scan(size,src,out);
+    }
   }
+}
   Kokkos::finalize();
+  return 0;
 }
